8ae51da0175d68906748512b8960bbe7403a1aa6
Pablo Fernandez <heelhook@littleq.net>
2013-03-10 20:17:41 -0400

    Initial import of rails4 generated code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cartogitphy::Application.config.secret_key_base = '9a4f1ee9c2a193c86dbf88b1fa5d34470907397fea09e35e2f3da8541fccb6c8c8e43f57726e6b177bb6479ac4105596c27d83fd482ab37180bda1c70e24e441'
